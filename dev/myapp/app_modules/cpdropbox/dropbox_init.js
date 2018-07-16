ca7b624f67a3b9311d8f11d023f14dff6820180a
hang <leehangbok2009@gmail.com>
2018-07-16 13:12:54 +0900

    modified dropbox_init

--- dev/myapp/app_modules/cpdropbox/dropbox_init.js
+++ dev/myapp/app_modules/cpdropbox/dropbox_init.js
@@ -9,29 +9,18 @@
 */
 
 // require('isomorphic-fetch');
-var Dropbox = require('dropbox').Dropbox;
-var dbutil = require('../db/db_util');
+const Dropbox = require('dropbox').Dropbox;
+const knex = require('../db/knex');
 
-module.exports = function(usr_session, callback){
+module.exports = function(user_session, callback){
 
-
-
-  dbutil.pool.executeQuery(function(con){
-    con.query('select * from user_token', function(err, result, fields){
-
-      if(err){
-        con.release();
-        throw err;
-      } else {
-        // var usr_access_token = result[0].Daccess;
-        var usr_access_token = 'kFb_ENWtmyUAAAAAAAABXmkgkMo381IwrSZdCoj2voMWz0dRlWPda7Caj0ivnG7X';
-        var dbx = new Dropbox({ accessToken: usr_access_token });
-
-        // dropbox api's util functions will be executed in this callback function
-        callback(dbx);
-        con.release();
-      }
-    });
+  knex.select('accessToken_d').from('DROPBOX_CONNECT_TB').where('userID', user_session.userID)
+  .then(function(rows){
+    const USER_ACCESS_TOKEN = rows[0].accessToken_d;
+    var client = new Dropbox({ accessToken: USER_ACCESS_TOKEN });
+    callback(client);
+  })
+  .catch(function(err){
+    console.log(err);
   });
-
 }
