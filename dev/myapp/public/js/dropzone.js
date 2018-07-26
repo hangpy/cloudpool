cf6bd3f79e28a75255d3be4747f58f0ff0b2182e
hang <leehangbok2009@gmail.com>
2018-07-26 14:58:11 +0900

    updqted dropzone.js and refined upload error by adding none displayed tag with name FolderID. add getScript functions to loadejs.js

---
+++ dev/myapp/public/js/dropzone.js
@@ -0,0 +1,88 @@
+
+$(function() {
+
+ var obj = $(".dropzone");
+ var area = obj.attr('name');
+ var drive = (function(area) {
+   switch (area) {
+     case 'google-area':
+       return 'google'
+       break;
+     case 'dropbox-area':
+       return 'dropbox'
+       break;
+     case 'box-area':
+       return 'box'
+       break;
+     case 'split-area':
+       return 'split'
+       break;
+   }
+ })(area);
+ var folderID = $(".current-folder").attr('name');
+
+ obj.on('dragenter', function(e) {
+   e.stopPropagation();
+   e.preventDefault();
+   $(this).css({
+     'border': '3px solid #028ee1',
+     'background-color': 'rgba(2, 142, 225, 0.3)'
+   });
+ });
+
+ obj.on('dragleave', function(e) {
+   e.stopPropagation();
+   e.preventDefault();
+   $(this).css({
+     'border': '3px dotted #8a8a8a',
+     'background-color': 'rgb(255, 255, 255)'
+   });
+ });
+
+ obj.on('dragover', function(e) {
+   e.stopPropagation();
+   e.preventDefault();
+ });
+
+ obj.on('drop', function(e) {
+   e.preventDefault();
+   $(this).css({
+     'border': '3px dotted #8a8a8a',
+     'background-color': 'rgb(255, 255, 255)'
+   });
+
+   var files = e.originalEvent.dataTransfer.files;
+   if (files.length < 1)
+     return;
+
+   F_FileMultiUpload(files, obj, drive, folderID);
+ });
+});
+
+// 각 드랍박스 인자 필요, url에다 각 드라이브 이름 전달
+function F_FileMultiUpload(files, obj, drive, folderID) {
+  var folderID = folderID == '' || folderID == undefined ? "" : folderID;
+  if (confirm(files.length + "개의 파일을 업로드 하시겠습니까?")) {
+    var data = new FormData();
+    for (var i = 0; i < files.length; i++) {
+      data.append('uploads_list', files[i]);
+    }
+    $.ajax({
+      url: drive + '/upload/'+ folderID,
+      type: 'POST',
+      data: data,
+      async: true,
+      processData: false,
+      contentType: false,
+      success: function(response) {
+        console.log("success");
+        Refresh();
+      },
+      complete: function(response) {
+        console.log("complete");
+        Refresh();
+      }
+
+    });
+  }
+}
--- dev/myapp/public/js/loadejs.js
+++ dev/myapp/public/js/loadejs.js
@@ -13,27 +13,44 @@
 $(function(){
 
   $('#split').click( function() {
-      $('.replace').load("split/folder/");
+      $('.replace').load("split/folder/", function(){
+        $.getScript('/js/dropzone.js', function(data, textStatus, jqxhr) {
+          console.log("load dropzone.js: " + textStatus);
+        });
+      });
   });
 
   $('#box').click( function() {
-      $('.replace').load("box/folder/");
+      $('.replace').load("box/folder/", function(){
+        $.getScript('/js/dropzone.js', function(data, textStatus, jqxhr) {
+          console.log("load dropzone.js: " + textStatus);
+        });
+      });
   });
 
   $('#dropbox').click( function() {
-      $('.replace').load("dropbox/folder");
-     //  for (int i = 0; i < 3; i++)
-     // {
-     //    alert( $("#Folder_"+i).attr('value') );
-     //    console.log($("#Folder_"+i).attr('value'));
-     // }
+      $('.replace').load("dropbox/folder", function(){
+        $.getScript('/js/dropzone.js', function(data, textStatus, jqxhr) {
+          console.log("load dropzone.js: " + textStatus);
+        });
+      });
   });
+
+  $('#google').click( function() {
+    $('.replace').load("google/folder/", function(){
+      $.getScript('/js/dropzone.js', function(data, textStatus, jqxhr) {
+        console.log("load dropzone.js: " + textStatus);
+      });
+    });
+  });
+
+
   $('.setting-drive').click( function() {
       $('.replace').load("setting/page-setting-drive");
   });
 
   $('#google').click( function() {
     $('.replace').load("google/folder/");
-});
+  });
 
 });
